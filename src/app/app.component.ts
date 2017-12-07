c10df0669f7b73664ebf64d7c5852801592ec8e7
Bohdan Donchenko <bohdan.donchenko@wolterskluwer.com>
2017-12-07 18:54:33 +0200

    added tests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+  title = 'app';
+
   constructor() {}
 
   ngOnInit() {}
