8103625a13e209f039087a2f14c7e635aab462e4
Bohdan Donchenko <bohdan.donchenko@wolterskluwer.com>
2017-12-07 13:52:29 +0200

    moved configs

--- configs/webpack.prod.js
+++ configs/webpack.prod.js
@@ -33,5 +33,30 @@ module.exports = webpackMerge(commonConfig, {
         minimize: false // workaround for ng2
       }
     })
-  ]
+  ],
+
+  devServer: {
+    contentBase: './dist',
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      'Access-Control-Allow-Headers':
+        'X-Requested-With, content-type, Authorization'
+    },
+    historyApiFallback: true,
+    compress: true,
+    quiet: false,
+    inline: true,
+    hot: false,
+    stats: 'minimal',
+    port: 3000,
+    overlay: {
+      errors: true
+    },
+    clientLogLevel: 'none',
+    watchOptions: {
+      aggregateTimeout: 50,
+      ignored: /node_modules/
+    }
+  }
 });
