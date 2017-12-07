29e3a10e9e7875234dda7382b693b9018681403a
Bohdan Donchenko <bohdan.donchenko@wolterskluwer.com>
2017-12-07 15:32:45 +0200

    package.json refactoring

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -2,7 +2,7 @@ import 'core-js/es6';
 import 'core-js/es7/reflect';
 import 'hammerjs/hammer';
 import 'reflect-metadata/Reflect';
-require('zone.js/dist/zone');
+import 'zone.js/dist/zone';
 
 if (process.env.ENV === 'production') {
   // Production
