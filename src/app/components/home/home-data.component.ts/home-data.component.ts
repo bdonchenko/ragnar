3af89f9f6dfc61d290666bf1352ad796b00485ac
Bohdan Donchenko <bohdan.donchenko@wolterskluwer.com>
2017-12-11 15:24:37 +0200

    added title prop

--- src/app/components/home/home-data.component.ts/home-data.component.ts
+++ src/app/components/home/home-data.component.ts/home-data.component.ts
@@ -7,6 +7,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 })
 export class HomeDataComponent {
   @Input() data: number;
+  @Input() title: string;
   @Output() onUpdated = new EventEmitter<boolean>();
 
   click() {
