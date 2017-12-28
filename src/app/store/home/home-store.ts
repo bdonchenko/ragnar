53501e63a23a931bcd1b295e31b1c45d773f944c
Bohdan Donchenko <bohdan.donchenko@wolterskluwer.com>
2017-12-28 14:40:14 +0200

    renamed store-item-observable to store-item

--- src/app/store/home/home-store.ts
+++ src/app/store/home/home-store.ts
@@ -1,12 +1,12 @@
-import { StoreItemObservable } from 'app/store/store-item-observable';
+import { StoreItem } from 'app/store/store-item';
 
 export class HomeStore {
-  private _counter = new StoreItemObservable<number>();
+  private _counter = new StoreItem<number>();
   get counter() {
     return this._counter;
   }
 
-  private _serverCounter = new StoreItemObservable<number>();
+  private _serverCounter = new StoreItem<number>();
   get serverCounter() {
     return this._serverCounter;
   }
--- src/app/store/store-item.ts
+++ src/app/store/store-item.ts
@@ -1,16 +1,16 @@
 import { Observable } from 'rxjs/Rx';
 import { Subject } from 'rxjs/Subject';
 
-export class StoreItemObservable<T> extends Subject<T> {
+export class StoreItem<T> extends Subject<T> {
   private _currentValue: T;
 
-  // constructor(initialValue?: T) {
-  //   super();
+  constructor(initialValue?: T) {
+    super();
     
-  //   if (initialValue !== undefined) {
-  //     this._currentValue = initialValue;
-  //   }
-  // }
+    if (initialValue !== undefined) {
+      this._currentValue = initialValue;
+    }
+  }
 
   get value$(): Observable<T> {
     return Observable.of(this._currentValue).concat(this);
