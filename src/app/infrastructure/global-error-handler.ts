269ce4d49841aad5befea25b693618a6488ba636
Bohdan Donchenko <bohdan.donchenko@gmail.com>
2018-01-18 13:10:45 -0600

    added description for global-error-handler

--- src/app/infrastructure/global-error-handler.ts
+++ src/app/infrastructure/global-error-handler.ts
@@ -1,11 +1,17 @@
 import { ErrorHandler, Injectable } from '@angular/core';
 
+/*
+Final implementation. 
+
+MUST NOT BE CHANGED during feature implementation.
+*/
+
 @Injectable()
 export class GlobalErrorHandler implements ErrorHandler {
   constructor() {}
   // tslint:disable-next-line:no-any
   handleError(error: any): void {
     // tslint:disable-next-line:no-console
-    console.log(error);
+    console.error(error);
   }
 }
