b3879777b02ef097f76dff692ba579bad518f036
Bohdan Donchenko <bohdan.donchenko@gmail.com>
2018-01-19 09:09:36 -0600

    added base repository implentation

--- src/app/repositories/base-repository.ts
+++ src/app/repositories/base-repository.ts
@@ -2,11 +2,31 @@ import { HttpClient } from '@angular/common/http';
 import { IJsonResponse } from 'app/repositories/i-json-response';
 import { Observable } from 'rxjs/Observable';
 
+/* 
+  Final implementation.
+
+  MUST NOT BE CHANGED During feature implementation.
+*/
+
 export abstract class BaseRepository {
-  
   constructor(private httpClient: HttpClient) {}
 
   get<T>(url: string): Observable<T> {
     return this.httpClient.get<IJsonResponse<T>>(url).map(data => data.results);
+  }
+
+  // tslint:disable-next-line:no-any
+  post(url: string, body?: any, options?: any): Observable<ArrayBuffer> {
+    return this.httpClient.post(url, body, options);
+  }
+
+  // tslint:disable-next-line:no-any
+  put(url: string, body?: any, options?: any): Observable<ArrayBuffer> {
+    return this.httpClient.put(url, body, options);
+  }
+
+  // tslint:disable-next-line:no-any
+  delete(url: string, options?: any) {
+    return this.httpClient.delete(url, options);
   }
 }
