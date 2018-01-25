c58c71033b245d08fe165b31bf85ddb2e2799ce6
Bohdan Donchenko <bohdan.donchenko@gmail.com>
2018-01-25 14:45:57 -0600

    removed redundant dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
-//import './polyfills';
 
 if (process.env.ENV === 'production') {
   enableProdMode();
