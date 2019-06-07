e543f9d1a03f9bd33ce3bf21cbc529ad9ad1db9a
Bohdan Donchenko <bohdan.donchenko@gmail.com>
2019-06-07 17:20:01 +0300

    Deps update. Angular 8

--- jest.config.js
+++ jest.config.js
@@ -2,24 +2,28 @@ module.exports = {
   preset: 'jest-preset-angular',
   globals: {
     'ts-jest': {
-      tsConfigFile: '<rootDir>/src/tsconfig.spec.json'
-    },
-    __TRANSFORM_HTML__: true
+      stringifyContentPathRegex: '\\.html?$',
+      tsConfig: '<rootDir>/src/tsconfig.spec.json'
+    }
   },
-  setupTestFrameworkScriptFile: '<rootDir>/src/test/jest-init.ts',
+  setupFilesAfterEnv: ['<rootDir>/src/test/jest-init.ts'],
   modulePaths: ['<rootDir>/src', '<rootDir>/node_modules'],
   roots: ['<rootDir>/src/'],
-  //setupFiles: ['<rootDir>/test/jest-pretest.ts'],
   moduleNameMapper: {
     'app/(.*)': '<rootDir>/src/app/$1',
-    'assets/(.*)': '<rootDir>/src/assets/$1'
+    'assets/(.*)': '<rootDir>/src/assets/$1',
+    '^@actions/(.*)$': 'app/actions/$1',
+    '^@components/(.*)$': 'app/components/$1',
+    '^@repositories/(.*)$': 'app/repositories/$1',
+    '^@services/(.*)$': 'app/services/$1',
+    '^@stores/(.*)$': 'app/stores/$1'
   },
   collectCoverageFrom: [
     'src/app/**/*.ts',
     '!**/*.spec.{js,ts}',
     '!**/node_modules/**',
     '!**/*.d.ts',
-    '!**/*.module.ts',
+    '!**/*.module.ts'
   ],
   collectCoverage: true,
   coverageDirectory: './build/coverage',
